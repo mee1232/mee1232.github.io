a89bdb15e057d76d9d37f332dd963ebb869205a6
mee1232 <75776299+mee1232@users.noreply.github.com>
2020-12-12 19:00:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const menu= document.querySelector('#menu');
 const links=document.querySelector('.na_menu');
-
+const navlogo=document.querySelector('#nbc')
 
 
 const mobilemenu=()=>
@@ -10,3 +10,47 @@ const mobilemenu=()=>
   
 };
 menu.addEventListener('click',mobilemenu);
+const highlightM =()=>
+{
+    const ele=document.querySelector('.highlight')
+    const hm=document.querySelector('#homepage')
+    const am=document.querySelector('#aboutpage')
+    const sm=document.querySelector('#servicepage')
+let scrollPos=window.scrollY
+if(window.innerWidth>960 && scrollPos<600)
+{
+    hm.classList.add('highlight')
+    am.classList.remove('highlight')
+    return
+}
+else if(window.innerWidth>960 && scrollPos<1400)
+{
+    hm.classList.remove('highlight')
+    am.classList.add('highlight')
+    sm.classList.remove('highlight')
+return
+}
+else if(window.innerWidth>960 && scrollPos<1400)
+{
+    am.classList.remove('highlight')
+    sm.classList.add('highlight')
+return
+}
+if((ele && window.innerWidth<960 && scrollPos<600) || ele)
+{
+ele.classList.remove('highlight')
+}
+}
+window.addEventListener('click',highlightM)
+window.addEventListener('scroll',highlightM)
+const hidem =()=>
+{
+    const mb=document.querySelector('.is-active')
+    if(window.innerWidth <= 768 && mb)
+    {
+        menu.classList.toggle('is-active')
+        links.classList.remove('active')
+    }
+}
+links.addEventListener('click',hidem)
+navlogo.addEventListener('click',hidem)
